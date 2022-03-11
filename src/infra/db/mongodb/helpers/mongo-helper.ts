f3b49ceba7cb94b960086ba891bea7c8cbde1114
Fernando <fernando39357@gmail.com>
2022-03-11 13:15:23 -0300

    feat: ensure AccountMongoRepository returns an account on success

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,4 +1,4 @@
-import { MongoClient } from 'mongodb'
+import { Collection, MongoClient } from 'mongodb'
 
 export const MongoHelper = {
   client: null as unknown as MongoClient,
@@ -7,5 +7,12 @@ export const MongoHelper = {
   },
   async disconnect (): Promise<void> {
     this.client.close()
+  },
+  getCollection (name: string): Collection {
+    return this.client.db().collection(name)
+  },
+  map (data: any): any {
+    const { _id, ...rest } = data
+    return { ...rest, id: _id.toHexString() }
   }
 }
