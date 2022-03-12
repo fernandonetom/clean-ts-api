a0d8e1a24bc1683a71b79f855d1cc6f8bfc03bda
Fernando <fernando39357@gmail.com>
2022-03-12 16:44:14 -0300

    refactor: move account repository to single folder

--- src/infra/db/mongodb/account-repository/account-repository.spec.ts
+++ src/infra/db/mongodb/account-repository/account-repository.spec.ts
@@ -1,5 +1,5 @@
-import { AccountMongoRepository } from './account'
-import { MongoHelper } from './helpers/mongo-helper'
+import { AccountMongoRepository } from './account-repository'
+import { MongoHelper } from '../helpers/mongo-helper'
 
 interface SutTypes{
   sut: AccountMongoRepository
