1496f881d45f1e775a757a2270908b0732c1b97b
Fernando <fernando39357@gmail.com>
2022-03-13 17:25:37 -0300

    feat: add LoginFactory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
   mongoUrl: process.env.MONGO_URL ?? 'mongodb://192.168.1.180:27017/clean-node-api',
-  port: process.env.PORT ?? 3000
+  port: process.env.PORT ?? 3000,
+  jwtSecret: process.env.JWT_SECRET ?? 'secreeet'
 }
