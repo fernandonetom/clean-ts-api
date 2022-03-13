88aa438abef61d5d8f2d5429b666a18af0a3ef1d
Fernando <fernando39357@gmail.com>
2022-03-13 10:45:04 -0300

    test: ensure CompareFieldValidation return an InvalidParamError if validation fails

---
+++ src/presentation/helpers/validators/compare-field-validation.spec.ts
@@ -0,0 +1,25 @@
+import { InvalidParamError } from '../../errors'
+import { CompareFieldValidator } from './compare-field-validation'
+
+interface SutTypes {
+  sut: CompareFieldValidator
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new CompareFieldValidator('any_field', 'any_field_compare')
+
+  return { sut }
+}
+
+describe('Required Field Validation', () => {
+  test('Should return a InvalidParamError if Validation fails', () => {
+    const { sut } = makeSut()
+
+    const error = sut.validate({
+      any_field: 'text',
+      any_field_compare: 'text_diff'
+    })
+
+    expect(error).toEqual(new InvalidParamError('any_field_compare'))
+  })
+})
