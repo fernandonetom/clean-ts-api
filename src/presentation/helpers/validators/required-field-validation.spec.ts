b6126b85014125ac802299766176122c629bd1e0
Fernando <fernando39357@gmail.com>
2022-03-13 10:41:15 -0300

    test: ensure RequiredFieldValidation return MissignParamError on error

---
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -0,0 +1,22 @@
+import { MissingParamError } from '../../errors'
+import { RequiredFieldValidation } from './required-field-validation'
+
+interface SutTypes {
+  sut: RequiredFieldValidation
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new RequiredFieldValidation('any_field')
+
+  return { sut }
+}
+
+describe('Required Field Validation', () => {
+  test('Should return a MissingParamError if Validation fails', () => {
+    const { sut } = makeSut()
+
+    const error = sut.validate({})
+
+    expect(error).toEqual(new MissingParamError('any_field'))
+  })
+})
