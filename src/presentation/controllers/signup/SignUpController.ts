21be4c9bd8d4db80ec9e8ab97dfbec0562798c36
Fernando <fernando39357@gmail.com>
2022-03-13 09:41:11 -0300

    refactor: remove unused tests

--- src/presentation/controllers/signup/SignUpController.ts
+++ src/presentation/controllers/signup/SignUpController.ts
@@ -1,5 +1,5 @@
 import { AddAccount, Controller, HttpResponse, HttpRequest, EmailValidator, Validation } from './SignUpProtocols'
-import { InvalidParamError, MissingParamError } from '../../errors'
+import { InvalidParamError } from '../../errors'
 import { badRequest, ok, serverError } from '../../helpers/HttpHelpers'
 
 export class SignUpController implements Controller {
@@ -13,13 +13,6 @@ export class SignUpController implements Controller {
     try {
       const error = this.validation.validate(httpRequest.body)
       if (error) return badRequest(error)
-
-      const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
-      for (const field of requiredFields) {
-        if (!httpRequest.body[field]) {
-          return badRequest(new MissingParamError(field))
-        }
-      }
 
       const { name, email, password, passwordConfirmation } = httpRequest.body
 
