4e8542a355dad1b16dd8a85e0755643796dad019
Fernando <fernando39357@gmail.com>
2022-03-16 10:49:46 -0300

    test: ensure AddSurveyUseCase call AddSurveyRepository with correct values

---
+++ src/data/usecases/add-survey/db-add-survey.spec.ts
@@ -0,0 +1,44 @@
+import { AddSurveyRepository } from '../../protocols/db/survey/add-survey-repository'
+import { AddSurveyModel } from './add-survey-protocols'
+import { DbAddSurvey } from './db-add-survey'
+
+const makeAddSurveyRepositoryStub = (): AddSurveyRepository => {
+  class AddSurveyRepositoryStub implements AddSurveyRepository {
+    async add (surveyData: AddSurveyModel): Promise<void> {
+      return await Promise.resolve()
+    }
+  }
+
+  return new AddSurveyRepositoryStub()
+}
+
+interface SutTypes {
+  sut: DbAddSurvey
+  addSurveyRepositoryStub: AddSurveyRepository
+}
+
+const makeSut = (): SutTypes => {
+  const addSurveyRepositoryStub = makeAddSurveyRepositoryStub()
+  const sut = new DbAddSurvey(addSurveyRepositoryStub)
+
+  return { sut, addSurveyRepositoryStub }
+}
+
+const makeFakeSurvey = (): AddSurveyModel => ({
+  question: 'any_qeustion',
+  answers: [{
+    answer: 'any_answer',
+    image: 'any_image'
+  }]
+})
+describe('DbAddSurvey UseCase', () => {
+  test('Should call AddSurveyRepository with correct values', async () => {
+    const { sut, addSurveyRepositoryStub } = makeSut()
+
+    const addSpy = jest.spyOn(addSurveyRepositoryStub, 'add')
+
+    await sut.add(makeFakeSurvey())
+
+    expect(addSpy).toHaveBeenCalledWith(makeFakeSurvey())
+  })
+})
